9aa2e965082e138d35b28efd8d33206735631c1c
Riku Pepponen <riku.pepponen@gmail.com>
2020-04-05 19:49:28 +0300

    Add a signup page

--- application.py
+++ application.py
@@ -24,3 +24,8 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return render_template("index.html")
+	
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
