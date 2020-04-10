d8a70db1f5661858dc54c9db03a458c7e5ebec4d
Riku Pepponen <riku.pepponen@gmail.com>
2020-04-11 01:15:00 +0300

    Differentiate between login/signup

--- application.py
+++ application.py
@@ -33,29 +33,34 @@ def signup():
 @app.route("/mypage", methods=["POST"])
 def mypage():
 
-    # REGISTER A NEW USER
-    # TODO: How to differentiate between login/signup form submissions?
-    
-    # Collect info from the signup form
-    input_email = request.form.get("email")
-    input_username = request.form.get("username")
-    input_password = request.form.get("password")
-    input_password2 = request.form.get("password2")
+    form_type = request.form.get("form_type")
+    if form_type == "login":
+        #TODO: LOG THE USER IN
+        print("LOGIN")
 
-    # Create the User class
-    class User(db.Model):
-        __tablename__ = "users"
-        id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-        email = db.Column(db.String, nullable=False)
-        username = db.Column(db.String, nullable=False) 
-        password = db.Column(db.String, nullable=False)
-        password2 = db.Column(db.String, nullable=False)
+    if form_type == "signup":
+        # REGISTER A NEW USER
+        
+        # Collect info from the signup form
+        input_email = request.form.get("email")
+        input_username = request.form.get("username")
+        input_password = request.form.get("password")
+        input_password2 = request.form.get("password2")
 
-    # Create an instance of the user class and fill it with the submitted form's data
-    user = User(email=input_email, username=input_username, password=input_password, password2=input_password2)
+        # Create the User class
+        class User(db.Model):
+            __tablename__ = "users"
+            id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+            email = db.Column(db.String, nullable=False)
+            username = db.Column(db.String, nullable=False) 
+            password = db.Column(db.String, nullable=False)
+            password2 = db.Column(db.String, nullable=False)
 
-    # Write the new user info to db
-    db.session.add(user)
-    db.session.commit()
+        # Create an instance of the user class and fill it with the submitted form's data
+        user = User(email=input_email, username=input_username, password=input_password, password2=input_password2)
+
+        # Write the new user info to db
+        db.session.add(user)
+        db.session.commit()
     
     return render_template("mypage.html", website_title=website_title)
