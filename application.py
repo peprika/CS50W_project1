a23c530a248ff0e78d9ba5584f6a3f06f3be430b
Riku Pepponen <riku.pepponen@gmail.com>
2020-04-12 20:32:42 +0300

    Allow GET method on mypage.html

--- application.py
+++ application.py
@@ -41,67 +41,73 @@ def signup():
         return render_template("signup.html", website_title=website_title)
 
 
-@app.route("/mypage", methods=["POST"])
+@app.route("/mypage", methods=["GET", "POST"])
 def mypage():
 
-    # Check if the user is logging or signing up
-    form_type = request.form.get("form_type")
-    if form_type == "login":
-        # LOG THE USER IN
+    if request.method == "GET":
+        # TODO: Check if user is logged in; if yes, allow to view mypage
+        return render_template("index.html", website_title=website_title)
         
-        # Get the login form data
-        input_username = request.form.get("username")
-        input_password = request.form.get("password")
+    if request.method == "POST":
+        # Check if the user is logging or signing up
+        form_type = request.form.get("form_type")
         
-        # Check that the username exists
-        username_exists = db.session.query(db.exists().where(User.username == input_username)).scalar()
-        if username_exists:
-        
-            # Check that the password is correct
-            user_info = db.session.query(User).filter(User.username == input_username).first()
-            if check_password_hash(user_info.password, input_password):
-                return render_template("mypage.html", website_title=website_title)
+        if form_type == "login":
+            # LOG THE USER IN
+            
+            # Get the login form data
+            input_username = request.form.get("username")
+            input_password = request.form.get("password")
+            
+            # Check that the username exists
+            username_exists = db.session.query(db.exists().where(User.username == input_username)).scalar()
+            if username_exists:
+            
+                # Check that the password is correct
+                user_info = db.session.query(User).filter(User.username == input_username).first()
+                if check_password_hash(user_info.password, input_password):
+                    return render_template("mypage.html", website_title=website_title)
+                else:
+                    return render_template("error.html", error_msg="Incorrect password. Go back and try again.")
             else:
-                return render_template("error.html", error_msg="Incorrect password. Go back and try again.")
-        else:
-            return render_template("error.html", error_msg="The username does not exist. Go back and try again.")
+                return render_template("error.html", error_msg="The username does not exist. Go back and try again.")
 
-    if form_type == "signup":
-        # REGISTER A NEW USER
-        
-        # Collect info from the signup form
-        input_email = request.form.get("email")
-        input_username = request.form.get("username")
-        input_password = request.form.get("password")
-        input_password2 = request.form.get("password2")
-        
-        # Check that form info is valid
-        email_regex = "^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
-        if (re.search(email_regex, input_email)) is None:
-            return render_template("error.html", error_msg="The email is invalid. Please go back and check you typed it correctly.")
-        if len(input_email) > 320:
-            return render_template("error.html", error_msg="The email is too long. Use max 320 characters.")
-        if len(input_username) > 16:
-            return render_template("error.html", error_msg="The username is too long. Use max 16 characters.")
-        if input_password != input_password2:
-            return render_template("error.html", error_msg="The passwords do not match. Go back and try again.")
-        
-        # Check that the email and username are available
-        email_exists = db.session.query(db.exists().where(User.email == input_email)).scalar()
-        username_exists = db.session.query(db.exists().where(User.username == input_username)).scalar()
-        if email_exists:
-            return render_template("error.html", error_msg="This email has already been registered. Go back and try again.")
-        if username_exists:
-            return render_template("error.html", error_msg="Username already exists. Go back and try again.")
-        
-        # Hash the password
-        pw_hash = generate_password_hash(input_password, method='pbkdf2:sha256', salt_length=8)
-        
-        # Create an instance of the user class and fill it with the submitted form's data
-        user = User(email=input_email, username=input_username, password=pw_hash)
+        if form_type == "signup":
+            # REGISTER A NEW USER
+            
+            # Collect info from the signup form
+            input_email = request.form.get("email")
+            input_username = request.form.get("username")
+            input_password = request.form.get("password")
+            input_password2 = request.form.get("password2")
+            
+            # Check that form info is valid
+            email_regex = "^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+            if (re.search(email_regex, input_email)) is None:
+                return render_template("error.html", error_msg="The email is invalid. Please go back and check you typed it correctly.")
+            if len(input_email) > 320:
+                return render_template("error.html", error_msg="The email is too long. Use max 320 characters.")
+            if len(input_username) > 16:
+                return render_template("error.html", error_msg="The username is too long. Use max 16 characters.")
+            if input_password != input_password2:
+                return render_template("error.html", error_msg="The passwords do not match. Go back and try again.")
+            
+            # Check that the email and username are available
+            email_exists = db.session.query(db.exists().where(User.email == input_email)).scalar()
+            username_exists = db.session.query(db.exists().where(User.username == input_username)).scalar()
+            if email_exists:
+                return render_template("error.html", error_msg="This email has already been registered. Go back and try again.")
+            if username_exists:
+                return render_template("error.html", error_msg="Username already exists. Go back and try again.")
+            
+            # Hash the password
+            pw_hash = generate_password_hash(input_password, method='pbkdf2:sha256', salt_length=8)
+            
+            # Create an instance of the user class and fill it with the submitted form's data
+            user = User(email=input_email, username=input_username, password=pw_hash)
 
-        # Write the new user info to db
-        db.session.add(user)
-        db.session.commit()
+            # Write the new user info to db
+            db.session.add(user)
+            db.session.commit()
     
     return render_template("mypage.html", website_title=website_title)
